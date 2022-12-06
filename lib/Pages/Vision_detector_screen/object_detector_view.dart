a5975492fde6d797328c86ee509fe961bc1d873c
thichuong <18520539@gm.uit.edu.vn>
2022-12-06 20:54:29 +0700

    Update object detector

---
+++ lib/Pages/Vision_detector_screen/object_detector_view.dart
@@ -0,0 +1,181 @@
+import 'dart:io' as io;
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
+import 'package:google_mlkit_translation/google_mlkit_translation.dart';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'camera_view.dart';
+import 'object_detector_painter.dart';
+import '/Model/SourceLang.dart';
+
+class ObjectDetectorView extends StatefulWidget {
+  @override
+  State<ObjectDetectorView> createState() => _ObjectDetectorView();
+}
+
+class _ObjectDetectorView extends State<ObjectDetectorView> {
+  late ObjectDetector _objectDetector;
+  final _onDeviceTranslatorFrom = OnDeviceTranslator(
+      sourceLanguage: TranslateLanguage.english,
+      targetLanguage: SourceLang.instance.TranslateLanguageFrom);
+  final _onDeviceTranslatorTo = OnDeviceTranslator(
+      sourceLanguage: TranslateLanguage.english,
+      targetLanguage: SourceLang.instance.TranslateLanguageTo);
+
+  bool _canProcess = false;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  String? _text;
+  String? _result;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initializeDetector(DetectionMode.stream);
+  }
+
+  @override
+  void dispose() {
+    _canProcess = false;
+    _objectDetector.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CameraView(
+      title: 'Object Detector',
+      customPaint: _customPaint,
+      text: _text,
+      onImage: (inputImage) {
+        processImage(inputImage);
+      },
+      result: _result,
+      onScreenModeChanged: _onScreenModeChanged,
+      initialDirection: CameraLensDirection.back,
+    );
+  }
+
+  void _onScreenModeChanged(ScreenMode mode) {
+    switch (mode) {
+      case ScreenMode.gallery:
+        _initializeDetector(DetectionMode.single);
+        return;
+
+      case ScreenMode.liveFeed:
+        _initializeDetector(DetectionMode.stream);
+        return;
+    }
+  }
+
+  void _initializeDetector(DetectionMode mode) async {
+    print('Set detector in mode: $mode');
+
+    // uncomment next lines if you want to use the default model
+    //final options = ObjectDetectorOptions(
+    //     mode: mode,
+    //     classifyObjects: true,
+    //     multipleObjects: true);
+    // _objectDetector = ObjectDetector(options: options);
+
+    // uncomment next lines if you want to use a local model
+    // make sure to add tflite model to assets/ml
+    final path = 'assets/ml/object_labeler.tflite';
+    final modelPath = await _getModel(path);
+    final options = LocalObjectDetectorOptions(
+      mode: mode,
+      modelPath: modelPath,
+      classifyObjects: true,
+      multipleObjects: true,
+    );
+    _objectDetector = ObjectDetector(options: options);
+
+    // uncomment next lines if you want to use a remote model
+    // make sure to add model to firebase
+    // final modelName = 'bird-classifier';
+    // final response =
+    //     await FirebaseObjectDetectorModelManager().downloadModel(modelName);
+    // print('Downloaded: $response');
+    // final options = FirebaseObjectDetectorOptions(
+    //   mode: mode,
+    //   modelName: modelName,
+    //   classifyObjects: true,
+    //   multipleObjects: true,
+    // );
+    // _objectDetector = ObjectDetector(options: options);
+
+    _canProcess = true;
+  }
+
+  Future<void> processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+    final List<String> listTextTo = <String>[];
+    final List<String> listTextFrom = <String>[];
+
+    final objects = await _objectDetector.processImage(inputImage);
+    for(final DetectedObject detectedObject in objects )
+      {
+        for (final Label label in detectedObject.labels) {
+          _text = _text! + '${label.text}\n';
+          var textTemp = await _onDeviceTranslatorFrom.translateText(label.text);
+          listTextFrom.add('$textTemp');
+          textTemp = await _onDeviceTranslatorTo.translateText(label.text);
+          listTextTo.add('$textTemp');
+        }
+      }
+    if (_text != null && _text!.length > 0) {
+      _text = _text!.substring(0, _text!.length - 1);
+    }
+    _result = _text;
+    if (inputImage.inputImageData?.size != null &&
+        inputImage.inputImageData?.imageRotation != null) {
+      final painter = ObjectDetectorPainter(
+          objects,
+          inputImage.inputImageData!.imageRotation,
+          inputImage.inputImageData!.size,
+          listTextFrom,
+          listTextTo
+      );
+      _customPaint = CustomPaint(painter: painter);
+    } else {
+      String text = 'Objects found: ${objects.length}\n\n';
+      for (final object in objects) {
+        text +=
+            'Object:  trackingId: ${object.trackingId} - ${object.labels.map((e) => e.text)}\n\n';
+      }
+      _text = text;
+      // TODO: set _customPaint to draw boundingRect on top of image
+      _customPaint = null;
+    }
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<String> _getModel(String assetPath) async {
+    if (io.Platform.isAndroid) {
+      return 'flutter_assets/$assetPath';
+    }
+    final path = '${(await getApplicationSupportDirectory()).path}/$assetPath';
+    await io.Directory(dirname(path)).create(recursive: true);
+    final file = io.File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(assetPath);
+      await file.writeAsBytes(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    }
+    return file.path;
+  }
+}
